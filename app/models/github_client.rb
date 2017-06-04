9810b60e3c98b61461b594c65a1773ff39721b12
Erin Fahy <efahy@stanford.edu>
2017-06-04 09:30:35 -0700

    Adds GithubClient model class

---
+++ app/models/github_client.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'github_api'
+
+class GithubClient
+  attr_reader :api
+
+  def initialize(org)
+    @api = Github.new do |c|
+      c.auto_pagination = true
+      c.oauth_token = Settings.GITHUB_OAUTH_TOKEN
+      c.user = org
+    end
+  end
+
+  def all_repos
+    @repos ||= api.repos.all
+  end
+
+  def all_repo_names
+    all_repos.map(&:name)
+  end
+
+  def repo_file_exists?(repo_name, path)
+    get_repo_file_data(repo_name, path).present?
+  end
+
+  def repo_file_contains?(repo_name, path, search_string)
+    file_data = get_repo_file_data repo_name, path
+    decoded_file = decoded_file_content(file_data.content)
+    content_contains?(decoded_file, search_string)
+  end
+
+  private
+
+  def repo_contents_client
+    @repo_contents ||= api.repos.contents
+  end
+
+  def get_repo_file_data(repo_name, path)
+    repo_contents_client.get repo: repo_name, path: path.to_s
+  rescue Github::Error::NotFound
+    NullResponse.new
+  end
+
+  def decoded_file_content(content)
+    Base64.decode64(content)
+  end
+
+  def content_contains?(content, search_string)
+    content.include?(search_string)
+  end
+end
---
+++ spec/models/github_client_spec.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe GithubClient do
+  subject(:client) { described_class.new('test-org') }
+
+  describe '#initialize' do
+    it 'creates an Github::Client instance' do
+      expect(client.api).to be_an Github::Client
+    end
+
+    it 'sets the clients user to the passed org' do
+      expect(client.api.user).to eq 'test-org'
+    end
+  end
+
+  describe '#all_repos' do
+    before do
+      response = file_fixture('all_repos').read
+      stub_request(:any, /api.github.com/).to_return(status: 200, body: response)
+    end
+
+    it 'creates an Github::ResponseWrapper instance' do
+      expect(client.all_repos).to be_an Github::ResponseWrapper
+    end
+
+    it 'responds to .map as an Enumerable' do
+      expect(client.all_repos.map).to be_kind_of Enumerable
+    end
+  end
+
+  describe '#all_repo_names' do
+    before do
+      response = file_fixture('all_repos').read
+      stub_request(:any, /api.github.com/).to_return(status: 200, body: response)
+    end
+
+    it 'returns all repo names as an array' do
+      expect(client.all_repo_names).to eq ['Hello-World', 'Hello-World2']
+    end
+  end
+
+  describe '#repo_file_exists?' do
+    describe 'with valid response' do
+      before do
+        response = file_fixture('repo_data_get_file').read
+        stub_request(:any, /api.github.com/).to_return(status: 200, body: response)
+      end
+
+      it 'returns true' do
+        expect(client.repo_file_exists?('fake', 'response')).to be true
+      end
+    end
+
+    describe 'with invalid response' do
+      before do
+        response = file_fixture('repo_data_get_file').read
+        stub_request(:any, /api.github.com/).to_return(status: 404, body: response)
+      end
+
+      it 'returns true' do
+        expect(client.repo_file_exists?('fake', 'response')).to be false
+      end
+    end
+  end
+
+  describe '#repo_file_contains?' do
+    describe 'with valid response' do
+      before do
+        response = file_fixture('repo_data_get_file_contents').read
+        stub_request(:any, /api.github.com/).to_return(status: 200, body: response)
+      end
+
+      it 'returns true' do
+        expect(client.repo_file_contains?('fake', 'Gemfile', 'rails')).to be true
+      end
+    end
+
+    describe 'with invalid response' do
+      before do
+        response = file_fixture('repo_data_get_file').read
+        stub_request(:any, /api.github.com/).to_return(status: 404, body: response)
+      end
+
+      it 'returns true' do
+        expect(client.repo_file_contains?('fake', 'Gemfile', 'rails')).to be false
+      end
+    end
+  end
+end
