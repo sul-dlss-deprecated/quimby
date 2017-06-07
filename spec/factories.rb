2e9e78ccac7e8406619d933d27003bc8f219772d
Erin Fahy <efahy@stanford.edu>
2017-06-06 21:39:36 -0700

    Adds repositories index page

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,19 @@ FactoryGirl.define do
     name 'Hello-World'
     organization 'octocat'
   end
+
+  factory :repository_index, class: Repository do
+    name 'Hello-World'
+    organization 'octocat'
+    url 'github.com'
+    language 'ruby'
+    is_rails 'true'
+    is_gem 'true'
+    has_capistrano 'true'
+    has_honeybadger 'true'
+    has_travis 'true'
+    has_coveralls 'true'
+    has_okcomputer 'true'
+    has_is_it_working 'true'
+  end
 end
