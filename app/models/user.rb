1c73ab2285734acf50ffd03b61af3bf5ccac17a4
Tony Zanella <tony.zanella@gmail.com>
2017-07-18 17:08:09 -0700

    saml_authenticatable is incompatible with database_authenticatable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable
-
-  def email_required?
-    false
-  end
-
-  def email_changed?
-    false
-  end
-
-  def will_save_change_to_email?
-    false
-  end
+  devise :saml_authenticatable
 end
