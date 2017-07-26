81a5347157b652cd56c166b1a6d64a596da1cd3b
Jon Robertson <jonrober@stanford.edu>
2017-07-26 13:22:00 -0700

    Remove all user login and tracking
    This will all be handled at the Apache level, in our case by shib,
    rather than by application-level logins.  We aren't using any of the
    devise data being stored for the user, so just pulling it all.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620172028) do
+ActiveRecord::Schema.define(version: 20170725221005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,20 +71,6 @@ ActiveRecord::Schema.define(version: 20170620172028) do
     t.boolean "pupgraded"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
