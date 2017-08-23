694b979946a2da9e9736b75be38edd3d9f14b54b
Erin Fahy <efahy@stanford.edu>
2017-08-23 08:19:14 -0700

    Adds pupgraded scope and filter to server index view

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -2,6 +2,6 @@
 
 class ServersController < ApplicationController
   def index
-    @servers = Server.order(:hostname)
+    @servers = Server.order(:hostname).filter(params.slice(:pupgraded))
   end
 end
--- app/models/server.rb
+++ app/models/server.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal:true
 
 class Server < ApplicationRecord
+  include Filterable
+
   has_many :deploy_environments
   has_many :repositories, through: :deploy_environments
+
+  scope :pupgraded, ->(pupgraded) { where(pupgraded: pupgraded) }
 end
