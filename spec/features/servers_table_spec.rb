66ed82d4678f8fb61f6335cef08d903b16008071
Erin Fahy <efahy@stanford.edu>
2017-08-23 08:19:14 -0700

    Adds pupgraded scope and filter to server index view

--- spec/features/servers_table_spec.rb
+++ spec/features/servers_table_spec.rb
@@ -16,4 +16,20 @@ describe 'Servers index table', type: :feature do
     visit servers_path
     expect(body).to have_link('server-prod-b', href: '/repositories/Hello-World')
   end
+
+  it 'gets correct results from true filter' do
+    create(:server_index)
+    visit servers_path
+    find('#pupgraded').select('true')
+    click_button 'Filter'
+    expect(body).to have_text('server-dev')
+  end
+
+  it 'gets correct results from false filter' do
+    create(:server_index)
+    visit servers_path
+    find('#pupgraded').select('false')
+    click_button 'Filter'
+    expect(body).not_to have_text('server-dev')
+  end
 end
